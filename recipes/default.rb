5c4245e17e1fe8a0439013a256d9343064baabe9
CPressland <mail@cpressland.io>
2016-06-13 13:44:47 +0100

    Setup basic users

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,23 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# --- Attribute Definitions
+uservars = node['henry-ghost']['users']
+
+
+# --- Add Required Users
+
+uservars.each do |createusers|
+  group createusers[:name] do
+    gid createusers[:gid]
+  end
+
+  user createusers[:name] do
+    home createusers[:home]
+    uid createusers[:uid]
+    gid createusers[:gid]
+    system false
+    shell '/bin/bash'
+  end
+end
