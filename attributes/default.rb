6ca8052500bbb34e13092a5fae70c79a0fc9474b
CPressland <mail@cpressland.io>
2016-06-13 13:49:59 +0100

    setup firewall config

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,3 +9,11 @@ default['appserver']['packages'] = %w(wget tar git autofs samba samba-client cif
 default['henry-ghost']['repositories']   = [
   { :reponame=>"epel", :repodescription=>"Extra Packages for Enterprise Linux", :repobaseurl=>"http://mirrors.ukfast.co.uk/sites/dl.fedoraproject.org/pub/epel/7/x86_64/", :repogpgkey=>"http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7"}
 ]
+
+default['firewalld']['enable_firewalld'] = true
+
+default['firewalld']['firewalld_services']       =  [
+  { :fwservice=>"ssh", :fwzone=>"public" }, # SSH
+  { :fwservice=>"http", :fwzone=>"public" }, # Caddy
+  { :fwservice=>"https", :fwzone=>"public" } # Caddy
+]
