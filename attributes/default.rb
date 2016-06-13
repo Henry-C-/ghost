a72e8d6d79c1363c1ca40a14d75824f6f638550a
CPressland <mail@cpressland.io>
2016-06-13 13:45:23 +0100

    Added Attributes file

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+
+default['henry-ghost']['users']          = [
+  { :name=>"apps", :uid=>1550, :gid=>1550, :home=>"/home/apps" },
+  { :name=>"henry", :uid=>1551, :gid=>1551, :home=>"/home/henry"}
+]
