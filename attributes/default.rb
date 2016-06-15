b49a6b1e8c28b10d5aab5ca57347b2aa3efe6214
Chris Pressland <mail@cpressland.io>
2016-06-15 19:08:32 +0100

    removed used packages

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['henry-ghost']['packages'] = %w(wget tar git autofs samba samba-client cifs-utils curl htop vim firewalld)
+default['henry-ghost']['packages'] = %w(wget tar git curl htop vim firewalld)
 
 default['henry-ghost']['repositories']   = [
   { :reponame=>"epel", :repodescription=>"Extra Packages for Enterprise Linux", :repobaseurl=>"http://mirrors.ukfast.co.uk/sites/dl.fedoraproject.org/pub/epel/7/x86_64/", :repogpgkey=>"http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-7"}
